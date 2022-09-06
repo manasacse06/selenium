027c450dce05a390e1238fe02451dae21fe3f20d
manasa <manasa@manasa-PC>
2022-09-06 12:06:24 -0500

    Testtestng02

---
+++ src/test/java/javaselenium/TestTestNG02.java
@@ -0,0 +1,29 @@
+package javaselenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestTestNG02 {
+	
+	@Test
+	
+	public void gogglesearch(){
+		WebDriver driver;
+		WebDriverManager.chromedriver().setup();
+		
+		driver=new ChromeDriver();
+		driver.get(" https://www.google.com");
+		WebElement searchBox = driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"));
+		searchBox.sendKeys("Google text");
+		searchBox.sendKeys(Keys.ENTER); 
+		
+		
+	}
+
+}
